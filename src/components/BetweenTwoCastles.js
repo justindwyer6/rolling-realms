9d87ca6558f961cd06ba3aa6ef7f8deb5e0ec6a3
Justin Dwyer <justindwyer6@gmail.com>
2020-04-07 22:58:47 -0700

    starting on Between Two Castles

---
+++ src/components/BetweenTwoCastles.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class BetweenTwoCastles extends React.Component {
+  render() {
+    return (
+      <>
+        <table className="betweenTwoCastles1">
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <table className="betweenTwoCastles2">
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default BetweenTwoCastles;
