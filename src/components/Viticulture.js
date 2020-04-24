1aed5efdb0f4ee4e7ca6b8ce953865b789f0a5d2
Justin Dwyer <justindwyer6@gmail.com>
2020-04-23 19:34:59 -0700

    quick refactor moving the renderX functions out of the actual render statement and into the class declaration (is that the right terminology?)

--- src/components/Viticulture.js
+++ src/components/Viticulture.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./Viticulture.scss";
 import { times } from "lodash";
 
+const rewards = ["heart", "heart", "coin", "coin", "pumpkin", "pumpkin"];
 class Viticulture extends React.Component {
-  render() {
-    const renderDie = i => (
+
+  renderDie(i) {
+    return (
       <img className="die" key={`${i+1}die`} src={`/assets/Links/dice-six-faces-${i+1}.png`} alt={`${i+1}die`}/>
     );
+  }
 
-    const rewards = ["heart", "heart", "coin", "coin", "pumpkin", "pumpkin"];
-    const renderRewards = i => (
+  renderRewards(i) {
+    return (
       <img className="reward" key={rewards[i]} src={`/assets/Links/${rewards[i]}.png`} alt={rewards[i]}/>
     );
+  }
 
-    const renderWineOrders = i => (
+  renderWineOrders(i) {
+    return(
       <div className="wineOrder" key={i}>
         <div className="wineGlass">11</div>
         <div className="stars">
@@ -21,13 +26,15 @@ class Viticulture extends React.Component {
           <img src="/assets/Links/star_victory.png" alt="star"/>
         </div>
       </div>
-    )
+    );
+  }
 
+  render() {
     return (
       <div className="grapes">
-        {times(6, (i) => renderDie(i))}
-        {times(6, (i) => renderRewards(i))}
-        {times(3, (i) => renderWineOrders(i))}
+        {times(6, (i) => this.renderDie(i))}
+        {times(6, (i) => this.renderRewards(i))}
+        {times(3, (i) => this.renderWineOrders(i))}
       </div>
     );
   }
