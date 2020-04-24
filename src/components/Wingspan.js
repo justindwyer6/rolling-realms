3c5c2c43bbf3db1a14cccfd6844bddc9dc890027
Justin Dwyer <justindwyer6@gmail.com>
2020-04-23 19:26:25 -0700

    starting on Wingspan layout

--- src/components/Wingspan.js
+++ src/components/Wingspan.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Wingspan.scss";
+import { times } from "lodash";
 
 class Wingspan extends React.Component {
-  render() {
+  renderBirds() {
     return (
       <div>hi</div>
+    );
+  }
+  render() {
+    return (
+      <div className="birds">
+        {times(3, () => this.renderBirds())}
+      </div>
+      // birds grid
+      //  feather w/ points h3
+      //  grid w/ offset rewards
     );
   }
 }
