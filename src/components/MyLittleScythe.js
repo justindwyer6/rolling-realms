8f4be9fcbe9a4202ee01158c51002ee7c4a830fe
Justin Dwyer <justindwyer6@gmail.com>
2020-04-28 17:52:53 -0700

    probably gonna throw these changes out because the code is outdated and doesn't seem to work. I'll give it another shot later though

--- src/components/MyLittleScythe.js
+++ src/components/MyLittleScythe.js
@@ -1,52 +1,20 @@
 import React from 'react';
+import "./MyLittleScythe.scss";
 
 class MyLittleScythe extends React.Component {
   render() {
     return (
-      <div className="myLittleScythe">
-        <ul id="grid" class="clear">
-          <li>
-              <div class="hexagon"></div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-6.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-1.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-5.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon">
-              <img src="/assets/Links/Charterstone_Icon_Pumpkin.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-2.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon"></div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-3.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-          <li>
-              <div class="hexagon">
-                <img src="/assets/Links/dice-six-faces-4.png" alt="" className="med-icon"/>
-              </div>
-          </li>
-        </ul>
+      <div id="myLittleScythe">
+        <div className="hexCrop">
+          <div className="hexContainer">
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+            <div className="hex"></div>
+          </div>
+        </div>
       </div>
     );
   }
