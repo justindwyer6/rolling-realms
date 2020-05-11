e91ac7e8aa81bf5520ef5dc762352d78e6b77275
Justin Dwyer <justindwyer6@gmail.com>
2020-05-11 09:13:59 -0700

    working on footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../App.css';
 import Minigame from './Minigame';
+import Round from './Round';
 import minigames from '../minigames';
+import { times } from "lodash";
 
 class App extends React.Component {
   state = { minigames };
@@ -30,9 +32,27 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-          {Object.keys(this.state.minigames).map(key => <Minigame key={key} index={key} minigame={this.state.minigames[key]} reorderGames={this.reorderGames} />)}
-      </div>
+      <>
+        <div className="header">
+          <img className="sm-logo" src="/assets/Links/stonemaier-logo.png" alt="Stonemaier Games Logo"/>
+          <div>
+            <h2 className="title">Rolling Realms <span>v</span>10</h2>
+            <h3>by Jamey Stegmaier</h3>
+          </div>
+          <p>
+            Select 3 realms per round for all players. On each of 9 turns, roll 2d6 for simultaneous use by all players. Use each die once, each in a different realm--you can’t use the same realm twice on the same turn. Most stars wins.
+          </p>
+        </div>
+        <div className="MinigamesC">
+            {Object.keys(this.state.minigames).map(key => <Minigame key={key} index={key} minigame={this.state.minigames[key]} reorderGames={this.reorderGames} />)}
+        </div>
+        <footer>
+          <div className="rounds">
+            {times(3, (i) => <Round key={i} round={i} />)}
+            <div className="total"></div>
+          </div>
+        </footer>
+      </>
     );
   }
 }
