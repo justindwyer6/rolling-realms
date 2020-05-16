8a60ac66cc2ed5ba29fd52b0875263878e6e948c
Justin Dwyer <justindwyer6@gmail.com>
2020-05-15 18:29:30 -0700

    scales down for smaller screens and adds a rule button for each round

--- src/components/App.js
+++ src/components/App.js
@@ -60,7 +60,8 @@ class App extends React.Component {
             <h3>by Jamey Stegmaier</h3>
           </div>
           <p>
-            Select 3 realms per round for all players. On each of 9 turns, roll 2d6 for simultaneous use by all players. Use each die once, each in a different realm--you can’t use the same realm twice on the same turn. Most stars wins. <span>Warning: Your progress will be lost if you refresh the page.</span>
+            Select 3 realms per round for all players. On each of 9 turns, roll 2d6 for simultaneous use by all players. Use each die once, each in a different realm--you can’t use the same realm twice on the same turn. Most stars wins.
+            <br></br><span>Warning: Your progress will be lost if you refresh the page.</span>
           </p>
           <div className="roundTotal">
             <p>
--- src/components/Round.js
+++ src/components/Round.js
@@ -4,6 +4,15 @@ import './Round.scss';
 import { times } from "lodash";
 
 class Round extends React.Component {
+  state = {
+    asideClasses: ""
+  };
+
+  showRules = () => {
+    let asideClasses = this.state.asideClasses;
+    asideClasses.includes("show") ? asideClasses = "" : asideClasses += " show";
+    this.setState({ asideClasses })
+  }
 
   renderInputs = (i) => {
     return (
@@ -27,6 +36,44 @@ class Round extends React.Component {
             {times(9, (i) => this.renderInputs(i))}
           </div>
         </div>
+        <aside className={this.state.asideClasses} onClick={this.showRules}>
+          Rules
+          <div className="rules">
+            <p>
+              • Pay 2<img className="inline-image" src="/assets/Links/pumpkin.png" alt="pumpkin"/> to adjust a die +/- 1
+            </p>
+            <p>
+              • Pay 3<img className="inline-image" src="/assets/Links/pumpkin.png" alt="pumpkin"/> to adjust a die +/-1; you may use it in a realm you’ve already used this turn
+            </p>
+            <p>
+              • Pay 2<img className="inline-image" src="/assets/Links/heart.png" alt="heart"/> if the non-adjusted dice show a pair to reuse a die
+            </p>
+            <p>
+              • Pay 3<img className="inline-image" src="/assets/Links/heart.png" alt="heart"/> to reuse a die
+            </p>
+            <p>
+              • Pay 2<img className="inline-image" src="/assets/Links/coin.png" alt="coin"/> if the non-adjusted dice sum is 7 to reuse a die
+            </p>
+            <p>
+              • Pay X<img className="inline-image" src="/assets/Links/coin.png" alt="coin"/> to gain a die of value X (1-6)
+            </p>
+            <p>
+              • Reused dice and extra #s can’t be used in the same realm(s) as other dice in the same turn
+            </p>
+            <p>
+              • Reuse dice as they were originally rolled
+            </p>
+            <p>
+              • Earned-but-unused resources are worth 0.1 stars each
+            </p>
+            <p>
+              • Resources do not carry over to the next round
+            </p>
+            <p>
+              • <a href="https://stonemaiergames.com/games/rolling-realms/" target="_blank" rel="noopener noreferrer">Details, Print, and FAQs</a>
+            </p>
+          </div>
+        </aside>
         <div className="roundTotal">
           <p>
             ROUND {this.props.round + 1}:
