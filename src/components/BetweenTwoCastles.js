613e8807488821324f075690a7057b0ad4bb66a6
Justin Dwyer <jdwyer@riskalyze.com>
2020-11-07 09:20:15 +0000

    updating packages and making BetweenTwoCastles a stateless functional component

--- src/components/BetweenTwoCastles.js
+++ src/components/BetweenTwoCastles.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./BetweenTwoCastles.scss";
 
-class BetweenTwoCastles extends React.Component {
+const BetweenTwoCastles = () => {
 
-  renderInput() {
+  const renderInput = () => {
     return (
       <div className="input">
         <input type="number" min="1" max="6"/>
@@ -11,59 +11,57 @@ class BetweenTwoCastles extends React.Component {
     );
   }
 
-  render() {
-    return (
-      <>
-        <p>Fill squares from the bottom up; #s on top must be lower than those below. When you complete a column, gain a bonus.</p>
-        <div className="betweenTwoCastlesC">
-          <div className="castle castle1">
-            <div className="no-border"></div>
-            <div className="rewards no-border">
-              <img src="/assets/Links/pumpkin.png" alt="pumpkin" className="reward"/>
-              <img src="/assets/Links/pumpkin.png" alt="pumpkin" className="reward overlay"/>
-            </div>
-            <div className="no-border"></div>
-            <div className="no-border"></div>
-            {this.renderInput()}
-            <div className="rewards no-border">
-              <img src="/assets/Links/heart.png" alt="reward" className="reward"/>
-              <img src="/assets/Links/heart.png" alt="reward" className="reward overlay"/>
-            </div>
-            <div className="rewards no-border">
-              <img src="/assets/Links/pumpkin.png" alt="reward" className="reward"/>
-            </div>
-            {this.renderInput()}
-            {this.renderInput()}
-            {this.renderInput()}
-            <div className="door">
-              <img src="/assets/Links/wooden-door.png" alt="door"/>
-            </div>
-            {this.renderInput()}
+  return (
+    <>
+      <p>Fill squares from the bottom up; #s on top must be lower than those below. When you complete a column, gain a bonus.</p>
+      <div className="betweenTwoCastlesC">
+        <div className="castle castle1">
+          <div className="no-border"></div>
+          <div className="rewards no-border">
+            <img src="/assets/Links/pumpkin.png" alt="pumpkin" className="reward"/>
+            <img src="/assets/Links/pumpkin.png" alt="pumpkin" className="reward overlay"/>
           </div>
+          <div className="no-border"></div>
+          <div className="no-border"></div>
+          {renderInput()}
+          <div className="rewards no-border">
+            <img src="/assets/Links/heart.png" alt="reward" className="reward"/>
+            <img src="/assets/Links/heart.png" alt="reward" className="reward overlay"/>
+          </div>
+          <div className="rewards no-border">
+            <img src="/assets/Links/pumpkin.png" alt="reward" className="reward"/>
+          </div>
+          {renderInput()}
+          {renderInput()}
+          {renderInput()}
+          <div className="door">
+            <img src="/assets/Links/wooden-door.png" alt="door"/>
+          </div>
+          {renderInput()}
+        </div>
 
-          <div className="castle castle2">
-            <div className="rewards no-border">
-              <img src="/assets/Links/coin.png" alt="reward" className="reward"/>
-              <img src="/assets/Links/heart.png" alt="reward" className="reward overlay"/>
-            </div>
-            <div className="rewards no-border">
-              <img src="/assets/Links/coin.png" alt="reward" className="reward"/>
-              <img src="/assets/Links/coin.png" alt="reward" className="reward overlay"/>
-            </div>
-            {this.renderInput()}
-            {this.renderInput()}
-            {this.renderInput()}
-            {this.renderInput()}
-            {this.renderInput()}
-            <div className="door">
-              <img src="/assets/Links/doorway.png" alt="door"/>
-            </div>
+        <div className="castle castle2">
+          <div className="rewards no-border">
+            <img src="/assets/Links/coin.png" alt="reward" className="reward"/>
+            <img src="/assets/Links/heart.png" alt="reward" className="reward overlay"/>
+          </div>
+          <div className="rewards no-border">
+            <img src="/assets/Links/coin.png" alt="reward" className="reward"/>
+            <img src="/assets/Links/coin.png" alt="reward" className="reward overlay"/>
+          </div>
+          {renderInput()}
+          {renderInput()}
+          {renderInput()}
+          {renderInput()}
+          {renderInput()}
+          <div className="door">
+            <img src="/assets/Links/doorway.png" alt="door"/>
           </div>
         </div>
-        <p className="margin20">Score 1 star per completed row (each castle has 3 rows).</p>
-      </>
-    );
-  }
+      </div>
+      <p className="margin20">Score 1 star per completed row (each castle has 3 rows).</p>
+    </>
+  );
 }
 
 export default BetweenTwoCastles;
