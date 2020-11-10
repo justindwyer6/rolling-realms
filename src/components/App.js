3c5b3b025a0c7303718a0f305be3f4dd5cb50551
Justin Dwyer <justindwyer6@gmail.com>
2020-11-10 22:56:47 +0000

    Hookifying App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Minigame from './Minigame';
 import Round from './Round';
 import Rules from './Rules';
 // import minigames from '../minigames';
-import rounds from '../rounds';
+import defaultRounds from '../rounds';
 import { times } from "lodash";
+const queryString = require('query-string');
 
-class App extends React.Component {
-  state = {
-    rounds: rounds,
-    missingMinigames: [],
-    footerClasses: "",
-    die1: 1,
-    die2: 6
-  };
+console.log(window.location.search);
+const parsed = queryString.parse(window.location.search);
+console.log(parsed);
+const App = () => {
+  // state = {
+  //   rounds: rounds,
+  //   missingMinigames: [],
+  //   footerClasses: "",
+  //   die1: 1,
+  //   die2: 6
+  // };
+  const [rounds, setRounds] = useState({ ...defaultRounds });
+  // const [missingMinigames, setMissingMinigames] = useState([]);
+  const [footerClasses, setFooterClasses] = useState("");
+  const [die1, setDie1] = useState(1);
+  const [die2, setDie2] = useState(6);
 
-  componentDidMount() {
-    const images = ["grape", "crushed-grape", "full-wine-glass", "empty-wine-glass"];
-    images.forEach(image => {
-      const img = new Image();
-      img.src = `/assets/${image}.png`;
-    });
-  }
+  // componentDidMount() {
+  //   const images = ["grape", "crushed-grape", "full-wine-glass", "empty-wine-glass"];
+  //   images.forEach(image => {
+  //     const img = new Image();
+  //     img.src = `/assets/${image}.png`;
+  //   });
+  // }
 
   // componentDidUpdate() {
   //   Object.keys(this.state.rounds).forEach((key, i) => {
@@ -44,15 +53,21 @@ class App extends React.Component {
   //   }
   // }
 
-  rollDice = () => {
+  const rollDice = () => {
     times(6, (i) => {
       let die1 = i;
       let die2 = ( 7 - i );
-      setTimeout(() => { this.setState({ die1, die2 }) }, (50 + i*15));
+      setTimeout(() => {
+        setDie1(die1);
+        setDie2(die2);
+      }, (50 + i*15));
     })
     let die1 = Math.ceil(Math.random()*6);
     let die2 = Math.ceil(Math.random()*6);
-    setTimeout(() => { this.setState({ die1, die2 }) }, 350);
+    setTimeout(() => {
+      setDie1(die1);
+      setDie2(die2);
+    }, 350);
   }
 
   // reorderGames = (roundNumber, minigame) => {
@@ -86,13 +101,13 @@ class App extends React.Component {
   //   this.setState({ rounds: newRoundOrder })
   // }
 
-  updateMinigame = (minigame, round) => {
-    const updatingRoundOrder = { ...this.state.rounds };
+  const updateMinigame = (minigame, round) => {
+    const updatingRoundOrder = { ...rounds };
     updatingRoundOrder[round].minigame = minigame;
-    this.setState({ rounds: updatingRoundOrder });
+    setRounds({ ...updatingRoundOrder });
   }
 
-  randomizeMinigames = () => {
+  const randomizeMinigames = () => {
     const allMinigames = [
       "Scythe",
       "Between Two Cities",
@@ -121,12 +136,12 @@ class App extends React.Component {
     }
 
     const shuffledMinigames = shuffle(allMinigames);
-    const roundsCopy = { ...this.state.rounds };
+    const roundsCopy = { ...rounds };
     // eslint-disable-next-line no-unused-vars
     Object.entries(roundsCopy).forEach(([key, value], i) => {
       value.minigame = shuffledMinigames[i];
     });
-    this.setState({ rounds: roundsCopy});
+    setRounds({ ...roundsCopy});
   }
 
   // randomizeRounds = () => {
@@ -141,106 +156,102 @@ class App extends React.Component {
   //   });
   // }
 
-  showRules = () => {
-    let footerClasses = this.state.footerClasses;
-    footerClasses.includes("show") ? footerClasses = "" : footerClasses += " show";
-    this.setState({ footerClasses });
+  const showRules = () => {
+    footerClasses.includes("show") ? setFooterClasses("") : setFooterClasses(" show");
   }
 
-  renderRoundTracker = (key, i) => {
+  const renderRoundTracker = (key, i) => {
     if (key === "1c" || key === "2c" || key === "3c") {
       return <Round key={i} round={i-1} />
     }
   }
 
-  render() {
-    return (
-      <>
-        <div className="header">
-          <img className="sm-logo" src="/assets/Links/stonemaier-logo.png" alt="Stonemaier Games Logo"/>
-          <div className="titleC">
-            <h2 className="title">Rolling Realms <span>v</span>10</h2>
-            <h3>by Jamey Stegmaier</h3>
-          </div>
+  return (
+    <>
+      <div className="header">
+        <img className="sm-logo" src="/assets/Links/stonemaier-logo.png" alt="Stonemaier Games Logo"/>
+        <div className="titleC">
+          <h2 className="title">Rolling Realms <span>v</span>10</h2>
+          <h3>by Jamey Stegmaier</h3>
+        </div>
+        <p>
+          Select 3 realms per round for all players. On each of 9 turns, roll 2d6 for simultaneous use by all players. Use each die once, each in a different realm--you can’t use the same realm twice on the same turn. Most stars wins.
+          <br></br><span className="warning">Warning: Your progress may be lost if you refresh the page or change the order of the rounds.</span>
+        </p>
+        <div className="gameTotal">
           <p>
-            Select 3 realms per round for all players. On each of 9 turns, roll 2d6 for simultaneous use by all players. Use each die once, each in a different realm--you can’t use the same realm twice on the same turn. Most stars wins.
-            <br></br><span className="warning">Warning: Your progress may be lost if you refresh the page or change the order of the rounds.</span>
+            TOTAL :
           </p>
-          <div className="gameTotal">
-            <p>
-              TOTAL :
-            </p>
-            <input type="number" name="total" />
-            <img src="/assets/Links/star_victory.png" alt="star"/>
-          </div>
-          <Rules key="headerRules" />
+          <input type="number" name="total" />
+          <img src="/assets/Links/star_victory.png" alt="star"/>
         </div>
+        <Rules key="headerRules" />
+      </div>
 
-        <div className="subheader">
-          <button className="standardButton" onClick={this.randomizeMinigames} >Randomize</button>
-          <button className="printButton" onClick={() => window.print()}>
-            <img className="printIcon" src="/assets/print.png" alt="print" />
-          </button>
-        </div>
-
-        <div className="MinigamesC">
-          {Object.keys(this.state.rounds).map((key, i) => {
-            return (
-              <>
-                <Minigame
-                  key={this.state.rounds[key].minigame}
-                  index={key}
-                  i={i}
-                  minigame={this.state.rounds[key].minigame}
-                  roundNumber={this.state.rounds[key].roundNumber}
-                  // reorderRounds={this.reorderRounds}
-                  updateMinigame={this.updateMinigame}
-                  missingMinigames={this.state.missingMinigames}
-                />
-                {this.renderRoundTracker(key, this.state.rounds[key].roundNumber)}
-              </>
-            );
-          })}
-        </div>
-
-        <button id="fixedRules" className={this.state.footerClasses} onClick={this.showRules}>
-          <h4>
-          	Rules
-          </h4>
-          <Rules key="floatRules" />
+      <div className="subheader">
+        <button className="standardButton" onClick={randomizeMinigames}>Randomize</button>
+        <button className="printButton" onClick={() => window.print()}>
+          <img className="printIcon" src="/assets/print.png" alt="print" />
         </button>
+      </div>
 
-        <button className="standardButton diceRoller" onClick={this.rollDice} >
-          <img
-            className="rolledDie"
-            src={`/assets/Links/dice-six-faces-${this.state.die1}.png`}
-            alt={`Rolled Die`}
-          />
-          <img
-            className="rolledDie"
-            src={`/assets/Links/dice-six-faces-${this.state.die2}.png`}
-            alt={`Rolled Die`}
-          />
-        </button>
+      <div className="MinigamesC">
+        {Object.keys(rounds).map((key, i) => {
+          return (
+            <>
+              <Minigame
+                key={rounds[key].minigame}
+                index={key}
+                i={i}
+                minigame={rounds[key].minigame}
+                roundNumber={rounds[key].roundNumber}
+                // reorderRounds={this.reorderRounds}
+                updateMinigame={updateMinigame}
+                // missingMinigames={missingMinigames}
+              />
+              {renderRoundTracker(key, rounds[key].roundNumber)}
+            </>
+          );
+        })}
+      </div>
 
-        <footer>
-          <div className="contribute">
-            <p>
-              <a href="https://stonemaiergames.com/games/rolling-realms/" target="_blank" rel="noopener noreferrer">Print-and-play other versions and find FAQs</a>
-            </p>
-            <hr></hr>
-            <p>This is a fan-made, open-source project.</p>
-            <p>
-              <a href="https://github.com/justin-dwyer/rolling-realms" target="_blank" rel="noopener noreferrer">Contribute on <img className="githubIcon" src="/assets/github-icon.png" alt="Github"/> Github</a>
-            </p>
-            <p>
-              <a href="https://github.com/justin-dwyer/rolling-realms" target="_blank" rel="noopener noreferrer">Report a Bug</a>
-            </p>
-          </div>
-        </footer>
-      </>
-    );
-  }
+      <button id="fixedRules" className={footerClasses} onClick={showRules}>
+        <h4>
+          Rules
+        </h4>
+        <Rules key="floatRules" />
+      </button>
+
+      <button className="standardButton diceRoller" onClick={rollDice} >
+        <img
+          className="rolledDie"
+          src={`/assets/Links/dice-six-faces-${die1}.png`}
+          alt={`Rolled Die`}
+        />
+        <img
+          className="rolledDie"
+          src={`/assets/Links/dice-six-faces-${die2}.png`}
+          alt={`Rolled Die`}
+        />
+      </button>
+
+      <footer>
+        <div className="contribute">
+          <p>
+            <a href="https://stonemaiergames.com/games/rolling-realms/" target="_blank" rel="noopener noreferrer">Print-and-play other versions and find FAQs</a>
+          </p>
+          <hr></hr>
+          <p>This is a fan-made, open-source project.</p>
+          <p>
+            <a href="https://github.com/justin-dwyer/rolling-realms" target="_blank" rel="noopener noreferrer">Contribute on <img className="githubIcon" src="/assets/github-icon.png" alt="Github"/> Github</a>
+          </p>
+          <p>
+            <a href="https://github.com/justin-dwyer/rolling-realms" target="_blank" rel="noopener noreferrer">Report a Bug</a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
