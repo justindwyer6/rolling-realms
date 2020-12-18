b2090b311ae8ba8dcff141a0413d1e4684288603
Justin Dwyer <jdwyer@riskalyze.com>
2020-12-18 21:51:53 +0000

    linting and refactoring functions in Round.js

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -1,40 +1,27 @@
-import React from "react";
-import Reward from "../Reward";
-import Rules from "../Rules";
-import "./Round.scss";
+import React, { useState } from "react";
 import { times } from "lodash";
+import Reward from "../Reward";
+// import Rules from "../Rules";
+import "./Round.scss";
 
-class Round extends React.Component {
-  state = {
-    asideClasses: "",
-    die1: 1,
-    die2: 6,
-  };
+const Round = ([round]) => {
+  const [rolledDice, setRolledDice] = useState([1, 6]);
 
-  rollDice = () => {
+  const rollDice = () => {
     times(6, (i) => {
-      let die1 = i;
-      let die2 = 7 - i;
       setTimeout(() => {
-        this.setState({ die1, die2 });
+        setRolledDice([i, 7 - i]);
       }, 50 + i * 15);
     });
-    let die1 = Math.ceil(Math.random() * 6);
-    let die2 = Math.ceil(Math.random() * 6);
     setTimeout(() => {
-      this.setState({ die1, die2 });
+      setRolledDice([
+        Math.ceil(Math.random() * 6),
+        Math.ceil(Math.random() * 6),
+      ]);
     }, 350);
   };
 
-  showRules = () => {
-    let asideClasses = this.state.asideClasses;
-    asideClasses.includes("show")
-      ? (asideClasses = "")
-      : (asideClasses += " show");
-    this.setState({ asideClasses });
-  };
-
-  renderInputs = (i) => {
+  const renderInputs = (i) => {
     return (
       <div key={i}>
         <input className="input1" type="number" name="die1" />
@@ -43,10 +30,10 @@ class Round extends React.Component {
     );
   };
 
-  renderRewards = (i, reward) => {
+  const renderRewards = (i, reward, round) => {
     return (
       <Reward
-        key={i + reward}
+        key={`round-${round}-${reward}-${i}`}
         i={i}
         reward={reward}
         rewardClasses="reward"
@@ -54,50 +41,48 @@ class Round extends React.Component {
     );
   };
 
-  render() {
-    return (
-      <div className="roundC">
-        <div className="turnsC">
-          <p className="sideways">TURNS</p>
-          <div className="turns">
-            {times(9, (i) => this.renderInputs(i))}
-          </div>
-        </div>
-        <button className="standardButton" onClick={this.rollDice}>
-          <img
-            className="rolledDie"
-            src={`/images/d6-${this.state.die1}.png`}
-            alt={`Rolled Die`}
-          />
-          <img
-            className="rolledDie"
-            src={`/images/d6-${this.state.die2}.png`}
-            alt={`Rolled Die`}
-          />
-        </button>
-        <div className="roundTotal">
-          <p>ROUND {this.props.round + 1}:</p>
-          <input type="number" name="roundTotal" />
-          <img src="/images/star.png" alt="star" />
-        </div>
-        <button className="ruleButton" onClick={this.showRules}>
-          <h4>Show Rules</h4>
-          <aside className={this.state.asideClasses}>
-            <Rules />
-          </aside>
-        </button>
-        <div className="rewards">
-          {times(12, (i) => this.renderRewards(i, "pumpkin"))}
-        </div>
-        <div className="rewards">
-          {times(12, (i) => this.renderRewards(i, "heart"))}
-        </div>
-        <div className="rewards">
-          {times(12, (i) => this.renderRewards(i, "coin"))}
+  return (
+    <div className="roundC">
+      <div className="turnsC">
+        <p className="sideways">TURNS</p>
+        <div className="turns">
+          {times(9, (i) => renderInputs(i))}
         </div>
       </div>
-    );
-  }
-}
+      <button
+        type="button"
+        aria-label="Roll Dice"
+        title="Roll Dice"
+        className="standardButton"
+        onClick={rollDice}
+      >
+        <img
+          className="rolledDie"
+          src={`/images/d6-${rolledDice[0]}.png`}
+          alt={`Die 1: ${rolledDice[0]}`}
+        />
+        <img
+          className="rolledDie"
+          src={`/images/d6-${rolledDice[1]}.png`}
+          alt={`Die 2: ${rolledDice[1]}`}
+        />
+      </button>
+      <div className="roundTotal">
+        <p>ROUND {round + 1}:</p>
+        <input type="number" name="roundTotal" />
+        <img src="/images/star.png" alt="star" />
+      </div>
+      <div className="rewards">
+        {times(12, (i) => renderRewards(i, "pumpkin"))}
+      </div>
+      <div className="rewards">
+        {times(12, (i) => renderRewards(i, "heart"))}
+      </div>
+      <div className="rewards">
+        {times(12, (i) => renderRewards(i, "coin"))}
+      </div>
+    </div>
+  );
+};
 
 export default Round;
