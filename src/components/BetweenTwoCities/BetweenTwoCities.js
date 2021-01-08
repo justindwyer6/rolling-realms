15b4bc8ff2fb4df80b1317b73056ec7361e1af12
Justin Dwyer <justindwyer6@gmail.com>
2021-01-08 22:52:09 +0000

    centering items vertically and horizontally, cleaning up BetweenTwoCities with layout for grid

--- src/components/BetweenTwoCities/BetweenTwoCities.js
+++ src/components/BetweenTwoCities/BetweenTwoCities.js
@@ -27,47 +27,59 @@ const BetweenTwoCities = () => {
         Fill a square; same #s can’t be orthogonally adjacent. Gain a
         bonus when you complete a row/column.
       </p>
-      <div className="gridGuideAlign">
-        <div className="betweenTwoCitiesC">
-          {times(9, (i) =>
-            (i + 1) % 3
-              ? renderCell(i)
-              : [
-                  renderCell(i),
-                  <div key={`rewards-${i}`} className="rewards">
-                    <img
-                      key={`reward-a-${i}`}
-                      className="reward"
-                      src={`/images/${rowRewards[`a${i}`]}.png`}
-                      alt={rowRewards[i - 4]}
-                    />
-                    <img
-                      key={`reward-b-${i}`}
-                      className="reward overlay"
-                      src={`/images/${rowRewards[`b${i}`]}.png`}
-                      alt={rowRewards[i - 3]}
-                    />
-                  </div>,
-                ],
-          )}
-          {times(3, (i) => (
+      {/* Render a 3x3 grid of inputs with two overlayed rewards appended to the end of each row */}
+      {times(9, (i) => {
+        console.log("index:");
+        console.log(i + 1);
+        console.log("Modulo:");
+        console.log((i + 1) % 3);
+        if ((i + 1) % 3 === 1) {
+          return [<b />, renderCell(i)];
+        }
+        if ((i + 1) % 3 === 2) {
+          return renderCell(i);
+        }
+        if ((i + 1) % 3 === 0) {
+          return [
+            renderCell(i),
             <div key={`rewards-${i}`} className="rewards">
               <img
                 key={`reward-a-${i}`}
                 className="reward"
-                src={`/images/${columnRewards[i]}.png`}
-                alt="reward"
+                src={`/images/${rowRewards[`a${i}`]}.png`}
+                alt={rowRewards[i - 4]}
               />
               <img
                 key={`reward-b-${i}`}
                 className="reward overlay"
-                src={`/images/${columnRewards[i]}.png`}
-                alt="reward"
+                src={`/images/${rowRewards[`b${i}`]}.png`}
+                alt={rowRewards[i - 3]}
               />
-            </div>
-          ))}
+            </div>,
+            <b />,
+          ];
+        }
+        return null;
+      })}
+      <b />
+      {times(3, (i) => (
+        <div key={`rewards-${i}`} className="rewards">
+          <img
+            key={`reward-a-${i}`}
+            className="reward"
+            src={`/images/${columnRewards[i]}.png`}
+            alt="reward"
+          />
+          <img
+            key={`reward-b-${i}`}
+            className="reward overlay"
+            src={`/images/${columnRewards[i]}.png`}
+            alt="reward"
+          />
         </div>
-      </div>
+      ))}
+      <b />
+      <b />
       <p>
         Score stars equal to the lowest of the other 2 realms this
         round. This # cannot be higher than filled squares here.
