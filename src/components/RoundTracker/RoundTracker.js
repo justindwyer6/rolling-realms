8cf1db0c1a6fc93b528cc8de4c22b74eef81327e
Justin Dwyer <justindwyer6@gmail.com>
2021-01-09 21:02:18 +0000

    finessing the roll tracker

--- src/components/RoundTracker/RoundTracker.js
+++ src/components/RoundTracker/RoundTracker.js
@@ -6,7 +6,8 @@ import "./RoundTracker.scss";
 const RoundTracker = ({ round }) => {
   const renderInputs = (i) => {
     return (
-      <div className="turn" key={`turn-${i}`}>
+      <div className="turn" key={`turn-${i + 1}`}>
+        <p>{i + 1}</p>
         <input className="roll-1" type="number" name="roll-1" />
         <input className="roll-2" type="number" name="roll=2" />
       </div>
@@ -33,15 +34,9 @@ const RoundTracker = ({ round }) => {
         <img src="/images/star.png" alt="star" />
       </div>
       {times(9, (i) => renderInputs(i))}
-      <div className="rewards">
-        {times(12, (i) => renderRewards(i, "pumpkin", round))}
-      </div>
-      <div className="rewards">
-        {times(12, (i) => renderRewards(i, "heart", round))}
-      </div>
-      <div className="rewards">
-        {times(12, (i) => renderRewards(i, "coin", round))}
-      </div>
+      {times(12, (i) => renderRewards(i, "pumpkin", round))}
+      {times(12, (i) => renderRewards(i, "heart", round))}
+      {times(12, (i) => renderRewards(i, "coin", round))}
     </div>
   );
 };
