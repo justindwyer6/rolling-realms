568cb987aafca82b8a6ea1996670abb1851e78aa
Justin Dwyer <justindwyer6@gmail.com>
2021-01-14 21:37:04 +0000

    adding functionality to the rewardTracker buttons

--- src/components/RewardTracker/RewardTracker.js
+++ src/components/RewardTracker/RewardTracker.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RewardTracker.scss";
 import pumpkin from "../../images/pumpkin.png";
 import heart from "../../images/heart.png";
 import coin from "../../images/coin.png";
 
 const RewardTracker = ({ rewardType }) => {
+  const [rewardsEarned, setRewardsEarned] = useState(0);
+  const [rewardsUsed, setRewardsUsed] = useState(0);
+
+  // const [heartsEarned, setHeartsEarned] = useState(0);
+  // const [heartsUsed, setHeartsUsed] = useState(0);
+
+  // const [coinsEarned, setCoinsEarned] = useState(0);
+  // const [coinsUsed, setCoinsUsed] = useState(0);
+
   let rewardSrc;
   if (rewardType === "pumpkin") {
     rewardSrc = pumpkin;
@@ -19,10 +28,20 @@ const RewardTracker = ({ rewardType }) => {
 
   return (
     <div className="RewardTracker">
-      <button type="button">-</button>
-      <div>12</div>
+      <button
+        type="button"
+        onClick={() => setRewardsUsed(rewardsUsed + 1)}
+      >
+        -
+      </button>
+      <div>{rewardsEarned - rewardsUsed}</div>
       <img src={rewardSrc} alt={rewardType} />
-      <button type="button">+</button>
+      <button
+        type="button"
+        onClick={() => setRewardsEarned(rewardsEarned + 1)}
+      >
+        +
+      </button>
     </div>
   );
 };
