e2fd6f34749860f07dc883e067ff80425d15ac19
Justin Dwyer <justindwyer6@gmail.com>
2021-01-18 21:13:11 +0000

    adds stories for the RewardTracker component

---
+++ src/components/RewardTracker/RewardTracker.stories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import RewardTracker from "./RewardTracker";
+
+export default {
+  title: "Example/RewardTracker",
+  component: RewardTracker,
+  argTypes: {},
+};
+
+const Template = (args) => <RewardTracker {...args} />; // eslint-disable-line react/jsx-props-no-spreading
+
+export const Pumpkin = Template.bind({});
+Pumpkin.args = {
+  rewardType: "pumpkin",
+};
+
+export const Heart = Template.bind({});
+Heart.args = {
+  rewardType: "heart",
+};
+
+export const Coin = Template.bind({});
+Coin.args = {
+  rewardType: "coin",
+};
