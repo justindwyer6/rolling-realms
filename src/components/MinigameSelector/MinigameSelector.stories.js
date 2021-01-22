c5ef4fec8f541e3ab8c39e73a2dd6c089d409fe6
Justin Dwyer <justindwyer6@gmail.com>
2021-01-22 22:15:50 +0000

    the story is rendering but I'm not sure how to get it to update

--- src/components/MinigameSelector/MinigameSelector.stories.js
+++ src/components/MinigameSelector/MinigameSelector.stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import MinigameSelector from "./MinigameSelector";
-import rounds from "../../rounds";
 
 export default {
   title: "Example/MinigameSelector",
@@ -8,16 +7,10 @@ export default {
   argTypes: {},
 };
 
-const updateMinigame = (minigame, round) => {
-  const updatingRoundOrder = { ...rounds };
-  updatingRoundOrder[round] = minigame;
-  setRounds({ ...updatingRoundOrder });
-};
-
 const Template = (args) => <MinigameSelector {...args} />; // eslint-disable-line react/jsx-props-no-spreading
 
-export const Default = Template.bind({});
-Default.args = {
+export const Scythe = Template.bind({});
+Scythe.args = {
+  index: "1a",
   minigameName: "Scythe",
-  updateMinigame: () => updateMinigame(),
 };
