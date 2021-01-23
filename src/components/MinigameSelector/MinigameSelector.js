095583d5feb2b385e037688c41e355bd2c975ef9
Justin Dwyer <justindwyer6@gmail.com>
2021-01-23 10:21:00 +0000

    testing out nesting the selector inside the header

--- src/components/MinigameSelector/MinigameSelector.js
+++ src/components/MinigameSelector/MinigameSelector.js
@@ -20,36 +20,31 @@ const MinigameSelector = ({
     }
   };
 
-  const renderMinigameHeader = () => (
+  return (
     <button
       className="minigameHeader"
       type="button"
       onClick={() => toggleMinigameSelector()}
     >
       {minigameName}
+      {isMinigameSelectorOpen ? (
+        <div className="minigameSelector">
+          {Object.keys(rounds).map((key) => {
+            return (
+              <button
+                className="minigameOption"
+                key={`${rounds[key]}-selector`}
+                type="button"
+                onClick={() => updateMinigame(rounds[key], index)}
+              >
+                {rounds[key]}
+              </button>
+            );
+          })}
+        </div>
+      ) : null}
     </button>
   );
-
-  if (!isMinigameSelectorOpen) {
-    return renderMinigameHeader();
-  }
-  return [
-    renderMinigameHeader(),
-    <div className="minigameSelector">
-      {Object.keys(rounds).map((key) => {
-        return (
-          <button
-            className="minigameOption"
-            key={`${rounds[key]}-selector`}
-            type="button"
-            onClick={() => updateMinigame(rounds[key], index)}
-          >
-            {rounds[key]}
-          </button>
-        );
-      })}
-    </div>,
-  ];
 };
 
 export default MinigameSelector;
