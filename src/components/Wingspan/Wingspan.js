9c54e985dc4a088cd5c9ac3e3e5734197d439c43
Justin Dwyer <justindwyer6@gmail.com>
2021-01-25 21:37:06 +0000

    might be temporary, but cleaning up the reward rendering logic

--- src/components/Wingspan/Wingspan.js
+++ src/components/Wingspan/Wingspan.js
@@ -3,35 +3,20 @@ import "./Wingspan.scss";
 import { times } from "lodash";
 
 const Wingspan = () => {
-  const rewards = [
-    "pumpkin",
-    "coin",
-    "star",
-    "coin",
-    "heart",
-    "star",
-    "heart",
-    "pumpkin",
-    "star",
-  ];
-
-  const renderBird = (i, wingspanOfBird) => {
+  const renderBird = (i, wingspanOfBird, rewardList) => {
     return (
       <div key={`bird-${i}`} className="bird">
         <span>{wingspanOfBird}</span>
         <div className="birdCells">
           {times(3, (j) => (
-            <div key={j}>
+            <div key={`bird-${i}-input-${j}`}>
               <input type="number" className="cell" />
+              <img
+                src={`/images/${rewardList[j]}.png`}
+                alt={rewardList[j]}
+                className="reward"
+              />
             </div>
-          ))}
-          {times(3, (j) => (
-            <img
-              key={j}
-              src={`/images/${rewards[j + i + 2 * i]}.png`}
-              alt={rewards[j + i]}
-              className="reward"
-            />
           ))}
         </div>
       </div>
@@ -44,9 +29,9 @@ const Wingspan = () => {
         Fill a square on any bird (left to right), then gain the bonus
         below that square
       </p>
-      {renderBird(1, 7)}
-      {renderBird(2, 12)}
-      {renderBird(3, 16)}
+      {renderBird(0, 7, ["pumpkin", "coin", "star"])}
+      {renderBird(1, 12, ["coin", "heart", "star"])}
+      {renderBird(2, 16, ["heart", "pumpkin", "star"])}
       <p>Score 1 star per complete bird whose sum = wingspan.</p>
     </>
   );
