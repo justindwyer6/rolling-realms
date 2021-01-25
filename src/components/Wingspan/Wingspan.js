92f479cfa285e37dc09789bca60352bab1d39a52
Justin Dwyer <justindwyer6@gmail.com>
2021-01-25 21:29:30 +0000

    getting rid of some just plain stupid calculations

--- src/components/Wingspan/Wingspan.js
+++ src/components/Wingspan/Wingspan.js
@@ -15,10 +15,10 @@ const Wingspan = () => {
     "star",
   ];
 
-  const renderBirds = (i) => {
+  const renderBird = (i, wingspanOfBird) => {
     return (
-      <div key={`bird${i}`} className="bird">
-        <p>{7 + i * 4 + (i ? 1 : 0)}</p>
+      <div key={`bird-${i}`} className="bird">
+        <span>{wingspanOfBird}</span>
         <div className="birdCells">
           {times(3, (j) => (
             <div key={j}>
@@ -44,7 +44,9 @@ const Wingspan = () => {
         Fill a square on any bird (left to right), then gain the bonus
         below that square
       </p>
-      {times(3, (i) => renderBirds(i))}
+      {renderBird(1, 7)}
+      {renderBird(2, 12)}
+      {renderBird(3, 16)}
       <p>Score 1 star per complete bird whose sum = wingspan.</p>
     </>
   );
