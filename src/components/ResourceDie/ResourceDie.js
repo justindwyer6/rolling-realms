32dcb75d67fd826ba66a5ef0907c8cdce47bf7a6
Justin Dwyer <justindwyer6@gmail.com>
2021-01-28 21:36:23 +0000

    setting up resourcedie

--- src/components/ResourceDie/ResourceDie.js
+++ src/components/ResourceDie/ResourceDie.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import Die from "../Die/Die";
+import Resource from "../Resource/Resource";
 
-const ResourceDie = ({ dieType, initialDieState, dieFace, classes }) => {
+const ResourceDie = ({
+  dieType,
+  initialDieState,
+  dieFace,
+  classes,
+}) => {
   return (
-    <Die
-      dieType={dieType}
-      initialDieState={initialDieState}
-      dieFace={dieFace}
-      classes={classes}
-     />
-    {/* <Resource /> */}
-  )
-}
+    <>
+      <Die
+        dieType={dieType}
+        initialDieState={initialDieState}
+        dieFace={dieFace}
+        classes={classes}
+      />
+      <Resource />
+    </>
+  );
+};
 
 export default ResourceDie;
