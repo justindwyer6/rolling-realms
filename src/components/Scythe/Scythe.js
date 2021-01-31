abe69d726c60f6a6686a23a18e7944586a13da37
Justin Dwyer <justindwyer6@gmail.com>
2021-01-31 10:11:36 +0000

    sets up top row of scythe with resourcedie

--- src/components/Scythe/Scythe.js
+++ src/components/Scythe/Scythe.js
@@ -29,29 +29,17 @@ const Scythe = () => {
 
   const scrambledDice = [5, 6, 2, 1, 4, 3];
 
-  const renderTopDie = (i) => {
+  const renderTopRow = (i, resources) => {
     return (
       <ResourceDie
         key={`scythe-top-d6-${i + 1}`}
         dieFace={i + 1}
         dieType="standard"
         classes="topDie"
-        resourceName="pumpkin"
-        isMinusResource
+        resourceName={resources[i]}
       />
     );
   };
-
-  // const renderTopRewards = (i, rewards) => {
-  //   return (
-  //     <img
-  //       className="topReward"
-  //       key={`scythe-top-reward-${rewards[i]}+${i}`}
-  //       src={`/images/${rewards[i]}.png`}
-  //       alt={rewards[i]}
-  //     />
-  //   );
-  // };
 
   const renderBottomDie = (i, dice) => {
     return (
@@ -95,7 +83,16 @@ const Scythe = () => {
         number, you may pay the bottom-row cost in the same column to
         mark it and gain its star.
       </p>
-      {times(6, (i) => renderTopDie(i))}
+      {times(6, (i) =>
+        renderTopRow(i, [
+          "coin",
+          "heart",
+          "pumpkin",
+          "coin",
+          "heart",
+          "pumpkin",
+        ]),
+      )}
       {times(6, (i) => renderBottomDie(i, scrambledDice))}
       {times(6, (i) => renderBottomRewards(i, bottomRewards))}
       {times(6, (i) => renderStars(i))}
