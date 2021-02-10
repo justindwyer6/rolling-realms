1baf567595f3156f0df62839fc7d8f059d379900
Justin Dwyer <justindwyer6@gmail.com>
2021-02-10 22:45:43 +0000

    removing the call to Board

--- src/components/Minigame/Minigame.js
+++ src/components/Minigame/Minigame.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Board from "../Board";
 import MinigameSelector from "../MinigameSelector/MinigameSelector";
 import Star from "../Star/Star";
 import "./Minigame.scss";
@@ -7,13 +6,13 @@ import "./Minigame.scss";
 const Minigame = ({ index, minigame, updateMinigame }) => {
   const stars = [1, 2, 3, 4, 5, 6];
   return (
-    <div className={`Minigame ${minigame.replace(/ +/g, "")}`}>
+    <div className={`Minigame ${minigame.displayName}`}>
       <MinigameSelector
         index={index}
         minigame={minigame}
         updateMinigame={updateMinigame}
       />
-      <Board minigame={minigame} />
+      {() => minigame()}
       <>
         {stars.map((star) => (
           <Star key={star} index={`${index}-star-${star}`} />
