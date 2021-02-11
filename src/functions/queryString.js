6be7b892634b50c8b9fb1e5d15101dbb6e1afe8b
Justin Dwyer <justindwyer6@gmail.com>
2021-02-11 08:15:38 +0000

    moving the function to set rounds based on querystring into the queryString.js file, and cleaning up the logic in the function

--- src/functions/queryString.js
+++ src/functions/queryString.js
@@ -1,4 +1,7 @@
 import qs from "query-string";
+import rounds from "../rounds";
+
+const roundKeys = Object.keys;
 
 export const setQueryStringWithoutPageReload = (queryStringValue) => {
   const newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}${queryStringValue}`;
@@ -16,4 +19,25 @@ export const getQueryStringValue = (
 ) => {
   const values = qs.parse(queryStringValue);
   return values[key];
+};
+
+export const setRoundsUsingQueryString = (setRounds) => {
+  const parsedQueryString = qs.parse(window.location.search);
+  Object.keys(parsedQueryString).forEach((key) => {
+    if (
+      // Stop function if the querystring doesn't have 9 entries
+      Object.keys(parsedQueryString).length !== 9 ||
+      // Stop function if an invalid key is in the querystring
+      !Object.keys(rounds).includes(key) ||
+      // Stop function if an invalid minigame is in the querystring
+      !Object.values(rounds)
+        .map((value) => value.name)
+        .includes(parsedQueryString[key])
+    ) {
+      return;
+    }
+    if (parsedQueryString[key] !== rounds[key]) {
+      setRounds({ ...parsedQueryString });
+    }
+  });
 };
