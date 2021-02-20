bf6ea50dbb4dafeecd84388c52f3b02ffc174df5
Justin Dwyer <justindwyer6@gmail.com>
2021-02-20 16:57:49 +0000

    fixing up the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,41 @@
 import React from "react";
 import "./Footer.scss";
+import githubIconSrc from "../../images/github-icon.png";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="contribute">
-        <p>
-          <a
-            href="https://stonemaiergames.com/games/rolling-realms/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Print-and-play other versions and find FAQs
-          </a>
-        </p>
-        <hr />
-        <p>This is a fan-made, open-source project.</p>
-        <p>
-          <a
-            href="https://github.com/justin-dwyer/rolling-realms"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contribute on{" "}
-            <img
-              className="githubIcon"
-              src="/images/github-icon.png"
-              alt="Github"
-            />{" "}
-            Github
-          </a>
-        </p>
-        <p>
-          <a
-            href="https://github.com/justin-dwyer/rolling-realms"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Report a Bug
-          </a>
-        </p>
-      </div>
+      <a
+        href="https://stonemaiergames.com/games/rolling-realms/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Print-and-play other versions and find FAQs
+      </a>
+      <hr />
+      <span>This is a fan-made, open-source project.</span>
+      <a
+        href="https://github.com/justin-dwyer/rolling-realms"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Contribute on{" "}
+        <img
+          className="githubIcon"
+          src={githubIconSrc}
+          alt="Github"
+        />
+        {"  "}
+        Github
+      </a>
+      <hr />
+      <a
+        href="https://github.com/justin-dwyer/rolling-realms"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Report a Bug
+      </a>
     </footer>
   );
 };
