e117c380b29a543b53ae3716828d7b932f9e14b8
Justin Dwyer <justindwyer6@gmail.com>
2021-02-25 20:37:38 +0000

    wired up the rollDice function and figured out a lot of stuff about keys and spreading and passing functions as arguments

--- src/components/DiceRoller/DiceRoller.js
+++ src/components/DiceRoller/DiceRoller.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import "./DiceRoller.scss";
 import getDieFace from "../../functions/getDieFace";
+import rollDice from "../../functions/rollDice";
 
 const DiceRoller = () => {
-  const [diceState, setDiceState] = useState([1, 6]);
-
-  const rollDice = () => {
-    console.log("heye");
-    setDiceState([3, 4]);
-  };
+  const [diceState, setDiceState] = useState([
+    { id: 1, dieFace: 1 },
+    { id: 2, dieFace: 6 },
+  ]);
 
   return (
     <button
       className="DiceRoller IconButton"
-      onClick={rollDice}
+      onClick={() => rollDice(diceState, setDiceState)}
       type="button"
     >
-      {diceState.map((dieFace) => (
+      {diceState.map((die) => (
         <img
-          className={`dieImage ${dieFace}`}
-          key={`${dieFace}die`}
-          src={getDieFace(dieFace)}
-          alt={`${dieFace}die`}
+          className={`dieImage ${die.dieFace}`}
+          key={`rolled-die-${die.id}`}
+          src={getDieFace(die.dieFace)}
+          alt={`${die.dieFace}die`}
         />
       ))}
     </button>
--- src/functions/rollDice.js
+++ src/functions/rollDice.js
@@ -1,14 +1,9 @@
-import d6Face1 from "../images/d6-1.png";
-import d6Face2 from "../images/d6-2.png";
-import d6Face3 from "../images/d6-3.png";
-import d6Face4 from "../images/d6-4.png";
-import d6Face5 from "../images/d6-5.png";
-import d6Face6 from "../images/d6-6.png";
+export default function rollDice(diceState, setDiceState) {
+  const dieFace1 = Math.ceil(Math.random() * 6);
+  const dieFace2 = Math.ceil(Math.random() * 6);
 
-const dice = [d6Face1, d6Face2, d6Face3, d6Face4, d6Face5, d6Face6];
-
-export default function rollDice() {
-  const dieFace = Math.ceil(Math.random() * 6);
-
-  return dice[dieFace];
+  setDiceState([
+    { ...diceState[0], dieFace: dieFace1 },
+    { ...diceState[1], dieFace: dieFace2 },
+  ]);
 }
