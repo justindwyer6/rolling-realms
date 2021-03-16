ddb417fcb9da624fa3c7efcfe9ecdd8b2ea32f9f
Justin Dwyer <justindwyer6@gmail.com>
2021-03-16 20:50:06 +0000

    using a package for my shuffle function to reduce code complexity and beginning to refactor the randomize function to fit the new data shape

--- src/utilities/randomizeMinigames.js
+++ src/utilities/randomizeMinigames.js
@@ -1,24 +1,8 @@
+import shuffle from "shuffle-array";
 import defaultRounds from "../models/rounds";
 
 export default function randomizeMinigames(rounds, setRounds) {
-  const allMinigames = Object.values(defaultRounds);
-
-  const shuffle = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex]; // eslint-disable-line no-param-reassign
-      array[randomIndex] = temporaryValue; // eslint-disable-line no-param-reassign
-    }
-    return array;
-  };
+  const allMinigames = defaultRounds.map((round) => round.minigame);
 
   const shuffledMinigames = shuffle(allMinigames);
   const roundsCopy = { ...rounds };
