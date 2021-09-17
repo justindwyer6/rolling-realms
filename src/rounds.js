a114e6f6dcf3bc470d90d2e1bab11ca12481ed7d
Justin Dwyer <justindwyer6@gmail.com>
2021-09-16 17:04:38 -0700

    add hook courtesy of abrad45: https://github.com/justindwyer6/rolling-realms/pull/50/files

--- src/rounds.js
+++ src/rounds.js
@@ -1,13 +1,13 @@
-const rounds = {
-  "1a": "Scythe",
-  "1b": "Between Two Cities",
-  "1c": "Charterstone",
-  "2a": "Between Two Castles",
-  "2b": "Viticulture",
-  "2c": "Euphoria",
-  "3a": "My Little Scythe",
-  "3b": "Tapestry",
-  "3c": "Wingspan",
-};
+const rounds = [
+  "Scythe",
+  "Between Two Cities",
+  "Charterstone",
+  "Between Two Castles",
+  "Viticulture",
+  "Euphoria",
+  "My Little Scythe",
+  "Tapestry",
+  "Wingspan",
+];
 
 export default rounds;
