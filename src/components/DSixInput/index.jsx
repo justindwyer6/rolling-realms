0c08bd2871dab6f5af8b35e9e96c8df64f32f5cd
Justin Dwyer <justindwyer6@gmail.com>
2022-01-09 17:02:30 -0800

    add clarifying comments

--- src/components/DSixInput/index.jsx
+++ src/components/DSixInput/index.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const useDSixInput = (initial = "") => {
   const [value, setValue] = useState(initial);
 
+  // onChange handler
   const updateValue = (e) => {
     const rawValue = e.target.value;
     const newValue = rawValue.replace(value, ""); // Remove the old value
@@ -10,13 +11,14 @@ const useDSixInput = (initial = "") => {
     setValue(newValue);
   };
 
+  // onKeyDown handler
   const preventInvalidKeyStrokes = (e) => {
     const { key } = e;
     const newInt = parseInt(key, 10);
 
     if (
       (Number.isNaN(newInt) || newInt < 1 || newInt > 6) &&
-      key.length === 1 // Don't prevent Backspace, Tab, etc.
+      key.length === 1 // Don't prevent "Backspace", "Tab", etc.
     ) {
       e.preventDefault();
     }
